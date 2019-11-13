6e6267dd3ca55bf2ed70c4d1a509e84391ec7eaa
Rollietjes <rollietjes@gmail.com>
2019-11-13 13:42:23 +0000

    Changed layout of the artist page

--- controllers/viewartist.js
+++ controllers/viewartist.js
@@ -9,24 +9,44 @@ const spotifyApi = require('../spotifyapi');
 const viewartist = {
   index(request, response) {
     const artistId = request.params.id;
+    let artistTopTracks = {};
+    let artistAlbums= {};
   
     let artist = {};
-    const result = spotifyApi.getArtist(artistId)
-    .then(function (data) {
-      artist = data.body;
-     
-    }, function (err) {
-      console.error(err);
-    }).then(function() { 
-      console.log(artist)
-      const viewData = {
-        title: 'artist',
-        artistId: artistId,
-        artist: artist
-      };
-      response.render('viewartist', viewData);
-    });     
+
+    
+    spotifyApi.getArtist(artistId)
+      .then(function(data) {
+        artist = data.body
+      })
+      .then(function() {
+        spotifyApi.getArtistTopTracks(artistId, "NL")
+        .then(function(data){
+          console.log("1")
+          artistTopTracks = data.body
+        }).then(function() {
+          spotifyApi.getArtistAlbums(artistId, { include_groups: "album"})
+          .then(function(data) {
+            artistAlbums = data.body;
+          }).then(function() {
+            console.log("2")
+            console.log(artist)
+            const viewData = {
+                  title: 'artist',
+                  artistId: artistId,
+                  artist: artist,
+                  artistTopTracks: artistTopTracks,
+                  artistAlbums: artistAlbums
+                };
+                response.render('viewartist', viewData);
+          })
+        })
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
   },
+  
 };
 
 module.exports = viewartist;
--- controllers/viewsong.js
+++ controllers/viewsong.js
@@ -3,14 +3,26 @@
 const uuid = require('uuid');
 const logger = require('../utils/logger');
 
+const spotifyApi = require('../spotifyapi');
+
 const viewsong = {
   index(request, response) {
     const songId = request.params.id;
-    logger.debug('viewsong id = ', songId);
-    const viewData = {
-      title: 'viewsong'
-    };
-    response.render('viewsong', viewData);
+    let track = {};
+    spotifyApi.getTrack(songId)
+      .then(function(data) {
+        track = data.body
+    }).then(function(){
+      console.log(track)
+      const viewData = {
+        title: 'viewsong',
+        track: track
+      };
+      response.render('viewsong', viewData);
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
   },
 };
 
